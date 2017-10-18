ec99a14d6a9656450bb08a17aff40f0cf164b78f
Yiduo Ke <yiduoke822@jhs185.org>
2017-10-17 23:11:54 -0400

    done

--- db_builder.py
+++ db_builder.py
@@ -19,17 +19,32 @@ with open('data/courses.csv') as csvfile:
     for row in reader:
         c.execute("INSERT INTO courses VALUES (?,?,?)", [row["code"], row["mark"], row["id"]])        
 c.execute("SELECT * FROM courses;")
-print(c.fetchall())
-print "\n"
 
 
 with open('data/peeps.csv') as csvfile:
     reader1 = csv.DictReader(csvfile)
     for row in reader1:
         c.execute("INSERT INTO peeps VALUES (?,?,?)", [row["name"], row["age"], row["id"]])
-c.execute("SELECT * FROM peeps;")
-print(c.fetchall())
 
+c.execute("SELECT name, mark FROM peeps, courses WHERE peeps.id=courses.id;")
+rows = c.fetchall()
+dict = {}
+
+current=rows[0][0]
+currentSum=0
+divideBy=0;
+for thing in rows:
+    if thing[0]!=current:
+        dict[current]=currentSum/divideBy
+        current=thing[0]
+        currentSum=0
+        divideBy=0
+    currentSum+=thing[1]
+    divideBy=divideBy+1
+
+c.execute("CREATE TABLE IF NOT EXISTS peeps_avg (name TEXT, average INTEGER);")
+for key in dict:
+    c.execute("INSERT INTO peeps_avg VALUES(?,?)",[key, dict[key]])
 #==========================================================
 db.commit() #save changes
 db.close()  #close database
